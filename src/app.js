a6dfdc61f1271aaab8949fc7f70841267eaab001
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2026-01-21 00:42:55 +0600

    Refactor CORS configuration to use an array of allowed origins

--- src/app.js
+++ src/app.js
@@ -12,17 +12,34 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
+
+// CORS
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.ciaomobile.eu",
+  "https://ciaomobile.eu",
+];
+
+// Add the environment variable to the list if it exists
+if (process.env.CLIENT_URL) {
+  allowedOrigins.push(process.env.CLIENT_URL);
+}
+
 app.use(
   cors({
-    origin:
-      process.env.CLIENT_URL ||
-      "http://localhost:3000" ||
-      "https://www.ciaomobile.eu" ||
-      "https://ciaomobile.eu",
-    // Restrict to frontend URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 app.use(express.json());
 
